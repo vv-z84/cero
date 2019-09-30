1be8eff7c57c5e4d60448f9af21bb7a0a28ae1f1
suika93 <43834700+suika93@users.noreply.github.com>
2019-09-30 03:10:52 +0000

    Add validation to Recipe do not have more than 5 ingredients (current game limitation)

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,5 @@ class Recipe < ApplicationRecord
   belongs_to :recipe_type
   has_many :recipe_ingredients, dependent: :destroy
 
-  validates :recipe_ingredients, length: { minimum: 2 }
+  validates :recipe_ingredients, length: { in: 2..5 }
 end
